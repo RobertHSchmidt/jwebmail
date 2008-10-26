4181bc545f49be454fd5579158eb90df8cf028d6
unsaved <unsaved@958dafde-dcd6-4a4b-9fd9-cb4d10be696a>
2008-10-26 15:27:52 +0000

    Add comments about non-intuitive namessrc/net/wastl/webmail/misc/Queue.java

--- trunk/src/net/wastl/webmail/misc/Queue.java
+++ trunk/src/net/wastl/webmail/misc/Queue.java
@@ -34,6 +34,7 @@ public class Queue  {
 
 
     public void queue(Object o) {
+        // Would be very intuitive if this method were named "push". 
         contents.addElement(o);
     }
 
@@ -42,6 +43,8 @@ public class Queue  {
     }
 
     public Object next() {
+        // Would be very intuitive if this method were named "pop". 
+        // "next" gives no indication that caller is changing the set contents.
         Object o=contents.firstElement();
         contents.removeElementAt(0);
         return o;
